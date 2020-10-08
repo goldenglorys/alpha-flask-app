e67ad538e5d0a69846ce760eca29cf121ead19db
Olusola Glory <olusolagloryolamide@gmail.com>
2020-10-08 13:47:20 +0100

    the art of databse system in flask implemented

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'  # tell flask which db is to be used
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self): # this will let know when new post is created through this model
+        return 'Blog post ' + str(self.id)
 
 # URL route in flask app
+
 
 all_posts = [
     {
         'title': 'Post 1',
-        'content': 'This is the content of post one'
+        'content': 'This is the content of post one',
+        'author': 'Edward'
     },
     {
         'title': 'Post 2',
