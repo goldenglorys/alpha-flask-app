7cc49e97d3ae60c8688e38e8a5dd49d1200b111a
Olusola Glory <olusolagloryolamide@gmail.com>
2020-10-08 13:04:57 +0100

    making use of templating and data from the controller

--- app.py
+++ app.py
@@ -4,10 +4,26 @@ app = Flask(__name__)
 
 # URL route in flask app
 
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post one'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post two'
+    }
+]
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/posts")
+def posts():
+    return render_template("posts.html", posts=all_posts)
 
 
 @app.route("/home/<string:name>", methods=['GET'])
